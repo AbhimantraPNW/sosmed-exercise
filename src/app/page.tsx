e1873b7157aea835e0021640e5b5389a64ca8d16
AbhimantraPNW <abhimantrapnw04@gmail.com>
2024-03-21 11:57:04 +0700

    feat: add layouting on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,37 @@
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Bell, HomeIcon, MessageSquareText, User } from "lucide-react";
+import { BellIcon, Home as HomeIcon, MessageSquareText, UserRound } from "lucide-react";
+import HeaderTweets from "./components/HeaderTweets";
+import CardWhoToFollow from "./components/CardWhoToFollow";
 
 export default function Home() {
   return (
-    <div className="text-3xl font-semibold text-blue-400">Hello World</div>
+    <main className="container mx-auto px-4">
+      <section className="grid grid-cols-5 pt-4">
+        {/* Sidebar */}
+        <div className="flex flex-col gap-4 ">
+          <Button variant="ghost" className="justify-normal">
+            <HomeIcon className="mr-2 h-4 w-4" /> Home
+          </Button>
+          <Button variant="ghost" className="justify-normal">
+            <BellIcon className="mr-2 h-4 w-4" /> Notification
+          </Button>
+          <Button variant="ghost" className="justify-normal">
+            <MessageSquareText className="mr-2 h-4 w-4" /> Message
+          </Button>
+          <Button variant="ghost" className="justify-normal">
+            <UserRound className="mr-2 h-4 w-4" /> Profile
+          </Button>
+        </div>
+        {/* End Sidebar */}
+
+        <div className="col-span-3">
+          <HeaderTweets />
+        </div>
+        
+        <div>
+          <CardWhoToFollow />
+        </div>
+      </section>
+    </main>
   );
 }
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,18 +1,16 @@
-
+import { Button } from "@/components/ui/button";
 import React from "react";
 
-const Navbar = () => {
-  
+const Navbar: React.FC = () => {
   return (
-    <nav>
-      <div className="container mx-auto px-4 bg-blue-300">
-        <div className="flex items-center justify-between py-4">
-          <h1 className="cursor-pointer text-2xl font-bold">TWITLALALA</h1>
-
-          <div className="hidden md:block">
-            <div className="flex cursor-pointer items-center gap-8 mr-10">
-              <h3>Profile</h3>
-            </div>
+    <nav className="bg-sky-400">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-between py-4 items-center">
+          <h1 className="cursor-pointer text-2xl text-white font-bold">TWITTOR</h1>
+          
+          <div className="flex gap-4">
+            <Button>Login</Button>
+            <Button>Register</Button>
           </div>
         </div>
       </div>
